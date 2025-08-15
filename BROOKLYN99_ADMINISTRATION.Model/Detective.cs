cfabe23f97b12046d6c0103537efb20b2a58a9c8
Máté Galavics <mategalavics@gmail.com>
2025-08-15 14:42:17 +0200

    Model Detective created
    Created the model class of the Detectives, added the main data types.

---
+++ BROOKLYN99_ADMINISTRATION.Model/Detective.cs
@@ -0,0 +1,17 @@
+﻿namespace BROOKLYN99_ADMINISTRATION.Model;
+
+public class Detective
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Rank  { get; set; }
+    public DateTime StartOfInvestigation { get; set; }
+
+    public Detective(string id, string name, string rank, DateTime startOfInvestigation)
+    {
+        Id = id;
+        Name = name;
+        Rank = rank;
+        StartOfInvestigation = startOfInvestigation;
+    }
+}
