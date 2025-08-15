2b7959be89aee667fd680b3ff51da9210d0eaa9c
Máté Galavics <mategalavics@gmail.com>
2025-08-15 15:12:27 +0200

    Model Report created
    Created the model of the reports, with the base data types.

---
+++ BROOKLYN99_ADMINISTRATION.Model/Report.cs
@@ -0,0 +1,15 @@
+﻿namespace BROOKLYN99_ADMINISTRATION.Model;
+
+public class Report
+{
+    public string Id { get; set; }
+    public DateTime DateOfCreation { get; set; }
+    public string Content { get; set; }
+
+    public Report(string id, DateTime dateOfCreation, string content)
+    {
+        Id = id;
+        DateOfCreation = dateOfCreation;
+        Content = content;
+    }
+}
